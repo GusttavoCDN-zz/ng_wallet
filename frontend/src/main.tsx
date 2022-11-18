9b1daaf4649c18c9fb6471a3dfbc63a914a583bc
Gustavo da Silva <gusttavo.x.santos@gmail.com>
2022-11-18 20:42:11 -0300

    chore(frontend): configured initial enviroment

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,12 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from 'react';
+import { App } from './App';
 
 const container = document.getElementById('root');
-const root = createRoot(container!);
+const root = createRoot(container as HTMLElement);
 
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
