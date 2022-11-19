0779c554ecb7606e87aafd8e3de63536d7414ba0
Gustavo da Silva <gusttavo.x.santos@gmail.com>
2022-11-19 15:49:28 -0300

    refactor(backend): find account controller and use case refactor to handle exceptions

--- backend/src/express/config/routes.ts
+++ backend/src/express/config/routes.ts
@@ -1,7 +1,6 @@
 import { Application } from 'express';
 import * as routes from '../routes';
 
-console.log(routes);
 export default (app: Application): void => {
   app.use('/api', ...Object.values(routes));
 };
