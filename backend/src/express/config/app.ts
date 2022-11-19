be9949cd142a050ffdbf4345091eb94b8c7851c5
Gustavo da Silva <gusttavo.x.santos@gmail.com>
2022-11-19 15:15:07 -0300

    feat(backend): added cors middleware

--- backend/src/express/config/app.ts
+++ backend/src/express/config/app.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import { cors } from '../middlewares/cors';
 import routes from './routes';
 
 const app = express();
 app.use(express.json());
+app.use(cors);
 
 routes(app);
 
