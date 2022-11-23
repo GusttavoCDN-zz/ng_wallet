7e869426f3e3988e9ea56d9468725f15237a8fb9
Gustavo da Silva <gusttavo.x.santos@gmail.com>
2022-11-23 10:47:16 -0300

    test(backend): test cors middleware

---
+++ backend/src/main/middlewares/cors.spec.ts
@@ -0,0 +1,15 @@
+import app from '../config/app';
+import request from 'supertest';
+
+describe('Cors MIddleware test', () => {
+  it('Should enable cors', async () => {
+    app.get('/test_cors', (req, res) => {
+      res.send();
+    });
+    await request(app)
+      .get('/test_cors')
+      .expect('access-control-allow-origin', '*')
+      .expect('access-control-allow-methods', '*')
+      .expect('access-control-allow-headers', '*');
+  });
+});
